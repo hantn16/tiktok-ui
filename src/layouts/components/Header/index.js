1b601c884bf711655e6e1218d7a5118460aebe24
theguy01 <hantn16@gmail.com>
2022-09-29 17:31:06 +0700

    Xây dựng UI phần Header #1

--- src/layouts/components/Header/index.js
+++ src/layouts/components/Header/index.js
@@ -1,11 +1,36 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faXmarkCircle,
+  faMagnifyingGlass,
+  faSpinner,
+} from '@fortawesome/free-solid-svg-icons';
+
 import styles from './Header.module.scss';
 import clsx from 'clsx';
+import images from '~/assets/images';
 
 function Header() {
   return (
     <header className={clsx(styles.wrapper)}>
-      <div className={clsx(styles.inner)}></div>
+      <div className={clsx(styles.inner)}>
+        <img src={images.logo} alt="TikTok" />
+        <div className={clsx(styles.search)}>
+          <input
+            type="text"
+            placeholder="Search accounts and videos"
+            spellCheck={false}
+          />
+          <button className={clsx('clear')}>
+            <FontAwesomeIcon icon={faXmarkCircle}></FontAwesomeIcon>
+          </button>
+          <FontAwesomeIcon className={clsx('loading')} icon={faSpinner} />
+          <button className={clsx(styles['search-btn'])}>
+            <FontAwesomeIcon icon={faMagnifyingGlass} />
+          </button>
+        </div>
+        <div className={clsx('actions')}></div>
+      </div>
     </header>
   );
 }
